21e75789f6a594125b80623ce2ea51057d84097e
root <root@LAPTOP-JL30QGKC.localdomain>
2023-04-17 22:03:25 +0300

    Edit and Update Post Actions

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,11 @@ Rails.application.routes.draw do
 
   get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  patch "/blog_posts/:id", to: "blog_posts#update"
+  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
+
   # blog_post_path(1)  => "/blog_posts/1"
   # blog_post_url(1)  => "http://localhost:3000/blog_posts/1"
-
   post "/blog_posts", to: "blog_posts#create", as: :blog_posts
 
 
